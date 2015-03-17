f91d609eed3f9e11fcfe91265691a00bec01f71a
jonobr1 <jonobrandel@gmail.com>
2015-03-17 13:15:04 -0700

    removed `window` dependency

--- src/requestAnimationFrame.js
+++ src/requestAnimationFrame.js
@@ -1,16 +1,18 @@
 define([], function() {
 
+  var root = root || this;
+
   /*
    * Requirified version of Paul Irish's request animation frame.
    * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
    */
 
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          window.oRequestAnimationFrame      ||
-          window.msRequestAnimationFrame     ||
+  return  root.requestAnimationFrame       ||
+          root.webkitRequestAnimationFrame ||
+          root.mozRequestAnimationFrame    ||
+          root.oRequestAnimationFrame      ||
+          root.msRequestAnimationFrame     ||
           function (callback) {
-            window.setTimeout(callback, 1000 / 60);
+            root.setTimeout(callback, 1000 / 60);
           };
 });
